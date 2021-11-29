3b8de67449b2d03f330022fac7ff0a2cbf5e7bbd
Andrew Ognew <han2332andrew@gmail.com>
2021-11-29 22:42:37 +0300

    Fix config and pom

--- src/main/java/com/istg/config/AppConfig.java
+++ src/main/java/com/istg/config/AppConfig.java
@@ -20,6 +20,7 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
         dataSource.setUrl("postgres://zcvfirvsnqdlev:40951b823a2b7c462939cddcdd798d50e1216730acf30346ef2d842331a2c9ad@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/do71815h64jo0");
         dataSource.setUsername("zcvfirvsnqdlev");
         dataSource.setPassword("40951b823a2b7c462939cddcdd798d50e1216730acf30346ef2d842331a2c9ad");
