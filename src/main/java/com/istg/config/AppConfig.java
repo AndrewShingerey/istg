6ce392527c49067e98fe88125c227e783e9bc6a2
Andrew Ognew <han2332andrew@gmail.com>
2021-11-30 00:24:41 +0300

    Fix db config

--- src/main/java/com/istg/config/AppConfig.java
+++ src/main/java/com/istg/config/AppConfig.java
@@ -21,9 +21,9 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setUrl("postgres://zcvfirvsnqdlev:40951b823a2b7c462939cddcdd798d50e1216730acf30346ef2d842331a2c9ad@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/do71815h64jo0");
+        dataSource.setUrl("postgres://vjxcwkiilteanj:e2d9316253c0eb936edd3d5d85eddc3eca429e1f39629913a96f2875bcbe4c9e@ec2-3-248-87-6.eu-west-1.compute.amazonaws.com:5432/d1bvs7c11s3o58\n");
         dataSource.setUsername("zcvfirvsnqdlev");
-        dataSource.setPassword("40951b823a2b7c462939cddcdd798d50e1216730acf30346ef2d842331a2c9ad");
+        dataSource.setPassword("e2d9316253c0eb936edd3d5d85eddc3eca429e1f39629913a96f2875bcbe4c9e");
         dataSource.setDriverClassName("org.postgresql.Driver");
         return dataSource;
     }
