624a110e2c8a9b12b5d5b3dc8b5fa76009671d6f
Andrew Ognew <han2332andrew@gmail.com>
2021-12-02 01:29:27 +0300

    fix config

---
+++ target/classes/database/populateData.sql
@@ -0,0 +1,39 @@
+INSERT INTO Roles
+(id_role, role_user)
+SELECT 1, 'USER'
+    WHERE
+    NOT EXISTS (
+        SELECT id FROM roles WHERE id = 1
+    );
+
+INSERT INTO Roles
+(id_role, role_user)
+SELECT 2, 'ADMIN'
+    WHERE
+    NOT EXISTS (
+        SELECT id FROM roles WHERE id = 2
+    );
+
+INSERT INTO TypeGame
+(id_type, TypeGame)
+SELECT 1, 'ADVENTURE'
+    WHERE
+    NOT EXISTS (
+        SELECT id_type FROM TypeGame WHERE id_type = 1
+    );
+
+INSERT INTO TypeGame
+(id_type, TypeGame)
+SELECT 2, 'ACTION'
+    WHERE
+    NOT EXISTS (
+        SELECT id_type FROM TypeGame WHERE id_type = 2
+    );
+
+INSERT INTO TypeGame
+(id_type, TypeGame)
+SELECT 3, 'INTELEGENT'
+    WHERE
+    NOT EXISTS (
+        SELECT id_type FROM TypeGame WHERE id_type = 3
+    );
